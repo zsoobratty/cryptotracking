507c82454ea99b32c4c8bee50b0f66e537d5af10
Ziad <ziad1991@hotmail.co.uk>
2020-09-25 13:07:54 +0100

    Add MONGOURI and connect to DB

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
+
+app.use(express.json());
+
+
+// DB config
+const db = require('./config/keys').MONGOURI;
+
+// Connect to DB
+mongoose.connect(db, 
+    { 
+        useNewUrlParser: true ,
+        useUnifiedTopology: true 
+    } 
+)
+    .then(() => console.log('MongoDB Connected!'))
+    .catch((err) => console.log(err))
 
 app.get("/", (req, res) => res.send('Hello World!'))
 
