e2113118007037813ff4490c45102acb0f134089
Ziad <ziad1991@hotmail.co.uk>
2020-09-28 14:32:54 +0100

    Destructure key import

--- middleware/requireToken.js
+++ middleware/requireToken.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
-const config = require('../config/keys')
+const {JWT_KEY} = require('../config/keys')
 
 const requireToken = async(req, res, next) => {
     const token = req.header('Authorization').replace('Bearer ', '')
-    const data = jwt.verify(token, config.JWT_KEY)
+    const data = jwt.verify(token, JWT_KEY)
     try {
         const user = await User.findOne({ _id: data._id, 'tokens.token': token })
         if(!user) {
