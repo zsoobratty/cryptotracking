ebf5efd47605597d8084383dcdedbd756251ff7e
Ziad <ziad1991@hotmail.co.uk>
2020-10-02 12:53:34 +0100

    Fix bug preventing access to signup route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import Home from './components/Home'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello Crypto
+      <Router>
+        <Switch>
+          <Route exact path="/" >
+              <Home />
+          </Route>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignIn = () => {
+    return (
+        <div>
+            This is the sign in page
+        </div>
+    )
+}
+
+export default SignIn
