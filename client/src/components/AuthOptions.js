6f3922e6543d99e08bc2569c75b7159a72332e37
Ziad <ziad1991@hotmail.co.uk>
2020-10-07 16:52:04 +0100

    Use context to change navbar based on user state

--- client/src/components/AuthOptions.js
+++ client/src/components/AuthOptions.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {useHistory} from 'react-router-dom'
+import UserContext from '../context/UserContext'
 
 const AuthOptions = () => {
+    const {userData, setUserData} = useContext(UserContext)
     const history = useHistory()
 
     const signUp = () => history.push('/signup')
     const signIn = () => history.push('/signin')
+    const logOut = () => {
+        setUserData({
+            token: undefined,
+            user: undefined
+        })
+        localStorage.setItem("token", "")
+    }
 
     return (
         <nav className='AuthOptions'>
-            <button onClick={signIn}><h1>Login</h1></button>
-            <button onClick={signUp}><h1>Register</h1></button>
+            {userData.user ? (
+                <button onClick={logOut}>Log out</button> 
+            ) : (
+                <div>
+                    <button onClick={signIn}><h1>Login</h1></button>
+                    <button onClick={signUp}><h1>Register</h1></button>
+                </div>
+            )
+            }
+        
         </nav>
     )
 }
