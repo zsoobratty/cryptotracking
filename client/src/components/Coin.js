7d16bad29acb54c70bb2ec636b666699d383da03
Ziad <ziad1991@hotmail.co.uk>
2020-10-09 00:57:24 +0100

    Fix coin styling with flex

--- client/src/components/Coin.js
+++ client/src/components/Coin.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Coin = ({coin}) => {
-    const { name, symbol, current_price, image } = coin
+    const { name, symbol, current_price, image, market_cap_rank } = coin
     return (
         <div className='Coin'>
+            <h1>{market_cap_rank}</h1>
             <img src={image} alt={name} width="50px" height="50px"/>
             <h2>{name}</h2> 
             <h3>{symbol.toUpperCase()}</h3>
--- client/src/components/Coins.js
+++ client/src/components/Coins.js
@@ -19,7 +19,7 @@ const Coins = () => {
 
     return (
         <div className='Coins-container'>
-            <h2>Here are the current top 100 coins</h2>
+            <h2 className='Coins-summary'>Here are the current top 100 coins</h2>
             <div className='Coins'>
             {coins.map((coin) => 
                 <Coin key={coin.id} coin={coin} />
