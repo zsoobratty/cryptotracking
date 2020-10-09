89258ad0d1612b026bedda2a32afd5204c172181
Ziad <ziad1991@hotmail.co.uk>
2020-10-09 01:01:12 +0100

    Conditionally render Profile link in NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useContext } from 'react'
 import {Link} from 'react-router-dom'
+import UserContext from '../context/UserContext'
 import AuthOptions from './AuthOptions'
 
 const NavBar = () => {
+    const {userData} = useContext(UserContext)
+
     return (
             <nav className='NavBar'>
                 <Link to="/"><h1 className='title'>CryptoTrack</h1></Link>
-                <Link to="/profile"><h3>Profile</h3></Link>
+                {userData.user && <Link to="/profile"><h3>Profile</h3></Link>}
                 <Link to="/coins"><h3>Top 100</h3></Link>
                 <AuthOptions />
             </nav>
