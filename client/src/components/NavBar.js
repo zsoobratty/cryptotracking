85d225200c0e1337f42d310c6cce549ae49f1deb
Ziad <ziad1991@hotmail.co.uk>
2020-10-19 15:59:50 +0100

    Implement search for coin with coinGecko API

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,19 +1,44 @@
-import React from 'react'
+import Axios from 'axios'
+import React, { useState } from 'react'
 import { useContext } from 'react'
 import {Link} from 'react-router-dom'
 import UserContext from '../context/UserContext'
 import AuthOptions from './AuthOptions'
+import M from 'materialize-css'
 
 const NavBar = () => {
+    const [query, setQuery] = useState('')
+    const [alert, setAlert] = useState('')
+    const [coin, setCoin] = useState({})
     const {userData} = useContext(UserContext)
+
+    const fetchData = async () => {
+        if(query !== '') {
+            const result = await Axios.get(`https://api.coingecko.com/api/v3/coins/${query}`)
+            console.log(result)
+            if(!result.data) {
+                // setAlert('Unable to find a coin under this name')
+                M.toast({html: `Unable to find a coin under the name of ${query}`, classes: "toast"})
+            }
+            setCoin(result.data)
+        } else {
+            M.toast({html: 'Please enter the name of a coin', classes:"toast"})
+        }
+    }
+
+    const handleCoinSearch = (e) => {
+        e.preventDefault()
+        fetchData()
+    }
+    
 
     return (
             <nav className='NavBar'>
                 <Link to="/"><h1 className='title'>CryptoTrack</h1></Link>
                 {userData.user && <Link to="/portfolio"><h3>Portfolio</h3></Link>}
                 <Link to="/coins"><h3>Top 100</h3></Link>
-                <form className='NavBar-search-form'>
-                    <input type="text" placeholder="Search for a coin"/>
+                <form className='NavBar-search-form' onSubmit={handleCoinSearch}>
+                    <input type="text" placeholder="Search for a coin" onChange={(e) => setQuery(e.target.value)} value={query}/>
                     <input type="submit" value="Search"/>
                 </form>
                 <AuthOptions />
